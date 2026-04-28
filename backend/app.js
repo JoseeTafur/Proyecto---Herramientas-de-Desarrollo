a64d8ce379b8f3df35b8cf53ba2a9b1d8c43225d
U23231612 <U23231612@utp.edu.pe>
2026-04-28 15:00:27 -0500

    feat: configurar servidor inicial

---
+++ backend/app.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+const PORT = 5000;
+
+app.get('/', (req, res) => {
+  res.send('Servidor funcionando');
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+});
